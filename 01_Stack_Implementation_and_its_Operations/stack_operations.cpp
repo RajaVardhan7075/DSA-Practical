b6fcd9fe2112347d1b0a440634d19e3f6f4872ae
RajaVardhan7075 <bobbadiraja43@gmail.com>
2026-07-31 01:30:37 +0530

    Implement stack operations in C++
    Added a C++ program to implement stack data structure using arrays. 
    Implemented basic stack operations such as push, pop, peek, and display with overflow and underflow condition handling.

---
+++ 01_Stack_Implementation_and_its_Operations/stack_operations.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 100
+
+class Stack {
+    int arr[MAX];
+    int top;
+   
+    public :
+        Stack() { top = -1;}
+   
+    bool isFull() {return top == MAX - 1;}
+    bool isEmpty() {return top = -1;}
+   
+    void push(int val) {
+        if (isFull()) { cout << "Stack is overflow\n" ; return ;}
+        arr[++top] = val;
+       
+        cout << val << " pushed into Stack.\n";
+    }
+   
+    void pop() {
+        if (isEmpty()) { cout << "Stack is underflow\n" ; return ;}
+       
+        cout << arr[top--] << " Popped from Stack.\n";
+    }
+   
+    void peek() {
+        if (isEmpty()) { cout << "Stack is empty.\n"; return ; }
+       
+        cout << "Top element: " << arr[top] << "\n";
+    }
+   
+    void traverse() {
+        if (isEmpty()) {cout << "Stack is empty.\n"; return ; }
+        cout << "Stack ( top to bottom ) : ";
+       
+        for (int i = top ; i >= 0 ; i-- ) {
+            cout << arr[i] << " ";
+        }
+       
+        cout << "\n";
+    }
+   
+    void search(int val) {
+        for (int i = top-1 ; i >= 0 ; i-- ) {
+            if (arr[i] == val) {
+                cout << val << " found at position " << (top - i + 1) << " from top.\n";
+                return ;
+            }
+        }
+        cout << val << " not found in stack.\n";
+    }
+};
+
+int main() {
+    Stack s;
+    int choice, val;
+   
+    do {
+        cout << "\n--- Stack Menu ---\n";
+        cout << "1.Push 2.Pop 3.Peek 4.Traverse 5.Search 0.Exit\n";
+        cout << "Enter Choice: ";
+        cin >> choice;
+       
+        switch (choice) {
+            case 1 : cout << "Enter value: "; cin >> val; s.push(val);
+                break;
+            case 2: s.pop(); break;
+            case 3: s.peek(); break;
+            case 4: s.traverse(); break;
+            case 5: cout << "Enter value to search: "; cin >> val; s.search(val) ; break ;
+        }
+    } while (choice != 0);
+   
+    return 0;
+}
