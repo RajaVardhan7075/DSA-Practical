5785e9fbce8776209d785f6f87c009b9f24d269e
BOBBADI RAJA VARDHAN <bobbadiraja43@gmail.com>
2026-09-03 08:42:04 +0530

    Add doubly linked list implementation with insertion

---
+++ 07_Doublely_Linked_list/insertion_types.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    Node* prev;
+    int data;
+    Node* next;
+};
+
+class Delete{
+    private:
+    Node* head;
+    Node* rev_head;
+    
+    public:
+    Delete(){
+        head = nullptr;
+        rev_head = nullptr;
+    }
+    
+    void insert_at_end(int value){
+        Node* newnode = new Node();
+        newnode-> prev = nullptr;
+        newnode-> data = value;
+        newnode-> next = nullptr;
+        
+        if(head == nullptr){
+            head = newnode;
+        }else{
+            Node* temp = head;
+            while(temp->next != nullptr){
+                temp = temp->next;
+            }
+            temp->next = newnode;
+            newnode->prev = temp;
+            rev_head = newnode;
+        }
+    }
+    
+    void display(){
+        Node* temp = head;
+        while(temp != nullptr){
+            cout<<temp->data<<"->";
+            temp = temp->next;
+        }
+        cout<<"Null\n";
+        
+        cout<<"Display using prev\n";
+        temp = rev_head;
+        while(temp != nullptr){
+            cout<<temp->data<<"->";
+            temp = temp->prev;
+        }
+        cout<<"Null\n";
+    }
+
+};
+
+int main(){
+    Delete d;
+    d.insert_at_end(10);
+    d.insert_at_end(20);
+    d.insert_at_end(30);
+    d.display();
+}
